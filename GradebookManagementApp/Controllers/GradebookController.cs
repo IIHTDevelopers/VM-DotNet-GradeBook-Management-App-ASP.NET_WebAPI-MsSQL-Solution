9cec938948b28adb8679c9038ac4f24169688e64
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-06 08:07:18 +0530

    Add files via upload

---
+++ GradebookManagementApp/Controllers/GradebookController.cs
@@ -0,0 +1,70 @@
+﻿using GradebookManagementApp.DAL.Interrfaces;
+using GradebookManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GradebookManagementApp.Controllers
+{
+    public class GradebookController : ApiController
+    {
+        private readonly IGradebookService _service;
+        public GradebookController(IGradebookService service)
+        {
+            _service = service;
+        }
+        public GradebookController()
+        {
+            // Constructor logic, if needed
+        }
+
+        [HttpPost]
+        [Route("api/Gradebook/CreateGrade")]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> CreateGrade([FromBody] Grade model)
+        {
+            var leaveExists = await _service.GetGradeById(model.StudentId);
+            var result = await _service.AddGrades(model);
+            return Ok(new Response { Status = "Success", Message = "Grade created successfully!" });
+        }
+
+
+        [HttpPut]
+        [Route("api/Gradebook/UpdateGrade")]
+        public async Task<IHttpActionResult> UpdateGrade([FromBody] Grade model)
+        {
+            var result = await _service.UpdateGradebook(model);
+            return Ok(new Response { Status = "Success", Message = "Grade updated successfully!" });
+        }
+
+
+        [HttpDelete]
+        [Route("api/Gradebook/DeleteGrade")]
+        public async Task<IHttpActionResult> DeleteGrade(long id)
+        {
+            var result = await _service.DeleteGradeById(id);
+            return Ok(new Response { Status = "Success", Message = "Grade deleted successfully!" });
+        }
+
+
+        [HttpGet]
+        [Route("api/Gradebook/GetGradeById")]
+        public async Task<IHttpActionResult> GetGradeById(long id)
+        {
+            var expense = await _service.GetGradeById(id);
+            return Ok(expense);
+        }
+
+
+        [HttpGet]
+        [Route("api/Gradebook/GetAllGrades")]
+        public async Task<IEnumerable<Grade>> GetAllGrades()
+        {
+            return _service.GetGradebook();
+        }
+    }
+}
---
+++ GradebookManagementApp/DAL/Services/Repository/GradebookRepository.cs
@@ -0,0 +1,86 @@
+﻿using GradebookManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GradebookManagementApp.DAL.Services.Repository
+{
+    public class GradebookRepository : IGradebookRepository
+    {
+        private readonly DatabaseContext _dbContext;
+        public GradebookRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Grade> AddGrades(Grade grade)
+        {
+            try
+            {
+                var result = _dbContext.Grades.Add(grade);
+                await _dbContext.SaveChangesAsync();
+                return grade;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<bool> DeleteGradeById(long id)
+        {
+            try
+            {
+                _dbContext.Grades.Remove(_dbContext.Grades.Single(a => a.StudentId == id));
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public List<Grade> GetGradebook()
+        {
+            try
+            {
+                var result = _dbContext.Grades.ToList();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<Grade> GetGradeById(long id)
+        {
+            try
+            {
+                return await _dbContext.Grades.FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public async Task<Grade> UpdateGradebook(Grade model)
+        {
+            var ex = await _dbContext.Grades.FindAsync(model.StudentId);
+            try
+            {
+                ex.StudentId = model.StudentId;
+                await _dbContext.SaveChangesAsync();
+                return ex;
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+    }
+}
